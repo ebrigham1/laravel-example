c28598e2d190a6e3aa04d6f7dc82773da3682314
Ethan Brigham <ebrigham1@gmail.com>
2018-02-11 11:50:34 -0500

    Small updates to activity log js

--- resources/assets/js/activityLog.js
+++ resources/assets/js/activityLog.js
@@ -13,6 +13,7 @@ Echo.channel('ActivityLog')
                 noActivitiesContainer.remove();
             });
         }
+        // Use the factory to add the activity log
         ActivityLogFactory.createAndPrepend(
             activityLogsContainer,
             activityLog.user_name,
@@ -22,24 +23,34 @@ Echo.channel('ActivityLog')
     });
 
 // If we have multiple pages set up infinite scroll
+// If we have more pages is stored as a data attribute on the activity log container
 if (activityLogsContainer.data('hasMorePages')) {
     let infiniteScroll = new InfiniteScroll('#activityLogs', {
+        // Path to get the next page
         path: function() {
             let nextPageIndex = this.pageIndex + 1;
+            // If we don't have a next page don't return a url
             if (activityLogsContainer.data('lastPageIndex') >= nextPageIndex) {
+                // Base url for next page stored as a data attribute on the activity log container
                 return activityLogsContainer.data('baseAjaxUrl') + '?page=' + nextPageIndex;
             }
         },
+        // Response type text for json
         responseType: 'text',
+        // Don't append becuase we will be doing it manually in the load function with the json we get back
         append: false,
+        // Don't affect browser history (this would be a nice addition at some point
         history: false,
-        status: '.page-load-status',
-        debug: true,
     });
+    // Infinite scroll load event (when data is loaded from the ajax endpoint
     infiniteScroll.on('load', function(response) {
+        // Parse the json data
         let data = JSON.parse(response);
+        // Get activity logs from it
         let activityLogs = data.data;
+        // Loop through activity logs
         for (let activityLog of activityLogs) {
+            // Use the factory to add the activity log element
             ActivityLogFactory.createAndAppend(
                 activityLogsContainer,
                 activityLog.user.name,
