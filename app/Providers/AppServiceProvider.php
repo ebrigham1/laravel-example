86f03c02b1b070c21a7eac8d56c028789b4e9138
Ethan Brigham <ebrigham1@gmail.com>
2019-02-18 12:08:05 -0500

    Adding dusk and phpunit to pre-commit hook

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,7 @@ use App\Observers\SectionObserver;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -45,6 +46,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (!$this->app->environment(['testing'])) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 }
