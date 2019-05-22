0ffd8751ba0247a9150b5564764663ded379e3a0
Ethan Brigham <ebrigham1@gmail.com>
2019-05-21 20:49:27 -0400

    Updating libraries

--- config/app.php
+++ config/app.php
@@ -166,7 +166,6 @@ return [
          * Package Service Providers...
          */
         Zizaco\Entrust\EntrustServiceProvider::class,
-        Sven\EnvProviders\ServiceProvider::class,
 
         /*
          * Application Service Providers...
